11648451a330fa7583a7e27a928e6cd8069387eb
ih4temyself <148211453+ih4temyself@users.noreply.github.com>
2025-07-02 21:40:33 +0300

    bot initial commit (start command)

---
+++ main.py
@@ -0,0 +1,15 @@
+import telebot
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+BOT_TOKEN = os.getenv('API_TOKEN')
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_help(message):
+    help_text = "blabalb"
+    bot.reply_to(message, help_text)
+
+bot.infinity_polling()
