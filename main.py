ace60909cf103af975fd8be968da52da5cc0742a
babamd <igordyak59@gmail.com>
2025-07-02 22:28:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import telebot
 from dotenv import load_dotenv
 import os
+from telebot import types
 
 load_dotenv()
-BOT_TOKEN = os.getenv('API_TOKE52N')
+BOT_TOKEN = os.getenv('API_TOKEN')
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
@@ -11,5 +12,15 @@ bot = telebot.TeleBot(BOT_TOKEN)
 def send_help(message):
     help_text = "blabalb"
     bot.reply_to(message, help_text)
+
+
+@bot.message_handler(commands=['weather'])
+def start(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton("clear", callback_data='weather_clear'))
+    markup.add(types.InlineKeyboardButton("rain", callback_data='weather_rain'))
+    markup.add(types.InlineKeyboardButton("thunder", callback_data='weather_thunder'))
+    bot.send_message(message.chat.id, "Выбери опцию:", reply_markup=markup)
+
 
 bot.infinity_polling()
